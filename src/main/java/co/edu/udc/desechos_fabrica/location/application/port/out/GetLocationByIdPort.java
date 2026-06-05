7fe7e2a5d53743411d89b612846da8f810831786
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-05 13:04:37 -0500

    feat(location/application): add UpdateLocationCommand and GetLocationByIdQuery to LocationApplicationMapper

--- src/main/java/co/edu/udc/desechos_fabrica/location/application/port/out/GetLocationByIdPort.java
+++ src/main/java/co/edu/udc/desechos_fabrica/location/application/port/out/GetLocationByIdPort.java
@@ -1,7 +1,7 @@
 package co.edu.udc.desechos_fabrica.location.application.port.out;
 
 import co.edu.udc.desechos_fabrica.location.domain.model.LocationModel;
-import co.edu.udc.desechos_fabrica.location.domain.valueobjects.LocationId;
+import co.edu.udc.desechos_fabrica.location.domain.valueobject.LocationId;
 import java.util.Optional;
 
 public interface GetLocationByIdPort {
