ed198e893578e30f552ab359efedcf1a8af079c5
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-05 18:46:44 -0500

    feat(location/infrastructure): add LocationEntity

---
+++ src/main/java/co/edu/udc/desechos_fabrica/location/infrastructure/adapter/persistence/entity/LocationEntity.java
@@ -0,0 +1,12 @@
+package co.edu.udc.desechos_fabrica.location.infrastructure.adapter.persistence.entity;
+
+public class LocationEntity {
+    Long id;
+    String name;
+    String address;
+    Long enterpriseId;
+    String coordinate;
+    String status;
+    String createdAt;
+    String updatedAt;
+}
