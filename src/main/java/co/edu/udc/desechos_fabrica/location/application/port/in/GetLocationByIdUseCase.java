01b2520ae0625274ba7d76dd45209dba26520ec8
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-06 11:07:27 -0500

    feat(location/application): add GetLocationByIdUseCase

---
+++ src/main/java/co/edu/udc/desechos_fabrica/location/application/port/in/GetLocationByIdUseCase.java
@@ -0,0 +1,9 @@
+package co.edu.udc.desechos_fabrica.location.application.port.in;
+
+import co.edu.udc.desechos_fabrica.location.domain.model.LocationModel;
+
+import java.util.Optional;
+
+public interface GetLocationByIdUseCase {
+    Optional<LocationModel> getById(Long id);
+}
