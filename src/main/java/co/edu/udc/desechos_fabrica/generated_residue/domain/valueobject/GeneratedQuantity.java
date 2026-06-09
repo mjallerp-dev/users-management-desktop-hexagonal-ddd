728abc65066972b57b3bd203e3d701f4bdf0b18d
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 00:38:05 -0500

    feat(residue/domain): add GeneratedQuantity

---
+++ src/main/java/co/edu/udc/desechos_fabrica/generated_residue/domain/valueobject/GeneratedQuantity.java
@@ -0,0 +1,23 @@
+package co.edu.udc.desechos_fabrica.generated_residue.domain.valueobject;
+
+import co.edu.udc.desechos_fabrica.generated_residue.domain.exception.InvalidGeneratedQuantityException;
+
+public record GeneratedQuantity(Double value, MeasurementUnit unit) {
+
+    public GeneratedQuantity {
+        if (value == null) {
+            throw InvalidGeneratedQuantityException.becauseValueIsNull();
+        }
+        if (unit == null) {
+            throw InvalidGeneratedQuantityException.becauseUnitIsNull();
+        }
+        if (value <= 0) {
+            throw InvalidGeneratedQuantityException.becauseMustBeGreaterThanZero();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value + " " + unit.name();
+    }
+}
