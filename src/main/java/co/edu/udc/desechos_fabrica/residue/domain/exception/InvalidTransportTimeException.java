692d1b0cc01f4224067362dfc078e8b645d32261
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 00:05:26 -0500

    feat(residue/domain): add InvalidTransportTimeException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/residue/domain/exception/InvalidTransportTimeException.java
@@ -0,0 +1,21 @@
+package co.edu.udc.desechos_fabrica.residue.domain.exception;
+
+import co.edu.udc.desechos_fabrica.shared.domain.DomainException;
+
+public class InvalidTransportTimeException extends DomainException {
+
+    private static final String MESSAGE_NULL = "The maximum transport time must not be null.";
+    private static final String MESSAGE_INVALID = "The maximum transport time must be strictly greater than zero minutes.";
+
+    private InvalidTransportTimeException(final String message) {
+        super(message);
+    }
+
+    public static InvalidTransportTimeException becauseValueIsNull() {
+        return new InvalidTransportTimeException(MESSAGE_NULL);
+    }
+
+    public static InvalidTransportTimeException becauseMustBeGreaterThanZero() {
+        return new InvalidTransportTimeException(MESSAGE_INVALID);
+    }
+}
