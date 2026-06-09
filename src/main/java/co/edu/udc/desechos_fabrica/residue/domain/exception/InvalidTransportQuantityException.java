fcfab1e19cd9b723a60003a71523db0519bbe3b5
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 00:05:17 -0500

    feat(residue/domain): add InvalidTransportQuantityException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/residue/domain/exception/InvalidTransportQuantityException.java
@@ -0,0 +1,21 @@
+package co.edu.udc.desechos_fabrica.residue.domain.exception;
+
+import co.edu.udc.desechos_fabrica.shared.domain.DomainException;
+
+public class InvalidTransportQuantityException extends DomainException {
+
+    private static final String MESSAGE_NULL = "The maximum transport quantity must not be null.";
+    private static final String MESSAGE_INVALID = "The maximum transport quantity must be greater than zero.";
+
+    private InvalidTransportQuantityException(final String message) {
+        super(message);
+    }
+
+    public static InvalidTransportQuantityException becauseValueIsNull() {
+        return new InvalidTransportQuantityException(MESSAGE_NULL);
+    }
+
+    public static InvalidTransportQuantityException becauseMustBeGreaterThanZero() {
+        return new InvalidTransportQuantityException(MESSAGE_INVALID);
+    }
+}
