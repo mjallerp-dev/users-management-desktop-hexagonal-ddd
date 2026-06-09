8e71ebfa7c7727f6efea18cd1c45f0d06c064441
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 10:29:18 -0500

    feat(residue/application): add SaveGeneratedResiduePort

---
+++ src/main/java/co/edu/udc/desechos_fabrica/generated_residue/application/port/out/SaveGeneratedResiduePort.java
@@ -0,0 +1,7 @@
+package co.edu.udc.desechos_fabrica.generated_residue.application.port.out;
+
+import co.edu.udc.desechos_fabrica.generated_residue.domain.model.GeneratedResidueModel;
+
+public interface SaveGeneratedResiduePort {
+    GeneratedResidueModel save(GeneratedResidueModel model);
+}
