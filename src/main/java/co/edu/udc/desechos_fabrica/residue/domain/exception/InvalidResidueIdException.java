b1b6d733378c01285b5dfb1ad7c5f95506c23b11
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 00:04:53 -0500

    feat(residue/domain): add InvalidResidueIdException

---
+++ src/main/java/co/edu/udc/desechos_fabrica/residue/domain/exception/InvalidResidueIdException.java
@@ -0,0 +1,21 @@
+package co.edu.udc.desechos_fabrica.residue.domain.exception;
+
+import co.edu.udc.desechos_fabrica.shared.domain.DomainException;
+
+public class InvalidResidueIdException extends DomainException {
+
+    private static final String MESSAGE_NULL = "The residue ID must not be null.";
+    private static final String MESSAGE_INVALID = "The residue ID must be a positive value greater than zero.";
+
+    private InvalidResidueIdException(final String message) {
+        super(message);
+    }
+
+    public static InvalidResidueIdException becauseValueIsNull() {
+        return new InvalidResidueIdException(MESSAGE_NULL);
+    }
+
+    public static InvalidResidueIdException becauseInvalidFormat() {
+        return new InvalidResidueIdException(MESSAGE_INVALID);
+    }
+}
