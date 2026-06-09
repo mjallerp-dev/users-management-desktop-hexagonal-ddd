6b05843bc7b5173469c353c6bf1e9cd13af8beb1
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 00:07:39 -0500

    feat(residue/domain): add ResidueId

---
+++ src/main/java/co/edu/udc/desechos_fabrica/residue/domain/valueobject/ResidueId.java
@@ -0,0 +1,25 @@
+package co.edu.udc.desechos_fabrica.residue.domain.valueobject;
+
+import co.edu.udc.desechos_fabrica.location.domain.exception.InvalidLocationIdException;
+
+
+public record ResidueId(Long value){
+
+    public ResidueId {
+        if (value == null){
+            throw InvalidLocationIdException.becauseValueIsNull();
+        }
+        validateGreaterThanZero(value);
+    }
+
+    private static void validateGreaterThanZero(final Long value) {
+        if (value <= 0) {
+            throw InvalidLocationIdException.becauseInvalidFormat();
+        }
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
+    }
+}
