1c765e25470e6070b70fa4e493648fe47c5256bd
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 17:10:49 -0500

    feat(residue/application): add GetGeneratedResidueByCodeService

---
+++ src/main/java/co/edu/udc/desechos_fabrica/generated_residue/application/service/GetGeneratedResidueByCodeService.java
@@ -0,0 +1,20 @@
+package co.edu.udc.desechos_fabrica.generated_residue.application.service;
+
+import co.edu.udc.desechos_fabrica.generated_residue.application.port.in.GetGeneratedResidueByCodeUseCase;
+import co.edu.udc.desechos_fabrica.generated_residue.application.port.out.FindGeneratedResidueByCodePort;
+import co.edu.udc.desechos_fabrica.generated_residue.domain.model.GeneratedResidueModel;
+import co.edu.udc.desechos_fabrica.generated_residue.domain.valueobject.GeneratedResidueCode;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class GetGeneratedResidueByCodeService implements GetGeneratedResidueByCodeUseCase {
+
+    private final FindGeneratedResidueByCodePort findGeneratedResidueByCodePort;
+
+    @Override
+    public Optional<GeneratedResidueModel> execute(String code) {
+        return findGeneratedResidueByCodePort.findByCode(new GeneratedResidueCode(code));
+    }
+}
