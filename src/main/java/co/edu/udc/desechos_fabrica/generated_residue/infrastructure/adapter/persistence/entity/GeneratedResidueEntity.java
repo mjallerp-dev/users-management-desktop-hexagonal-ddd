d6370664d67dbcac5ea2eb14469223e046420158
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-06-09 23:58:01 -0500

    feat(residue/infrastructure): add GeneratedResidueEntity

---
+++ src/main/java/co/edu/udc/desechos_fabrica/generated_residue/infrastructure/adapter/persistence/entity/GeneratedResidueEntity.java
@@ -0,0 +1,12 @@
+package co.edu.udc.desechos_fabrica.generated_residue.infrastructure.adapter.persistence.entity;
+
+import java.time.LocalDateTime;
+
+public class GeneratedResidueEntity {
+    private Long id;
+    private Long residueId;
+    private String code;
+    private Double generatedQuantity;
+    private LocalDateTime generationDate;
+
+}
